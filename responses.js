e741543f1752d740a1cbdf284923d89f359c521c
AmanKaushik77 <kaushikaman166@gmail.com>
2022-10-19 18:18:42 -0700

    added more responses and handled invalid responses

--- responses.js
+++ responses.js
@@ -10,24 +10,26 @@ function getBotResponse(input){
             productType = 1;
             
             return "you chose watch, what is your budget?";
-        }else if (input == "Computer"){
+        }else if (input == "laptop"){
             productType = 2;
-            return "you chose computer, what is your budget?";
+            return "you chose laptop, what is your budget?";
         }else if (input == "phone" || input == "phones" || input == "iphone"){
             productType = 3;
 
             return "you chose phone, what is your budget?";
         }else if (input == "tablet" || input == "ipad"){
             productType = 4;
-            
             return "you chose tablet, what is your budget?";
+        }else{
+            count = 0;
+            return "Sorry, didn't seem to understand, please try again :)"
         }
     }
     // getting budget 
     if (count == 1){
         var budget = parseInt(input);
         count ++;
-        
+        // budget for watch **needs work**
         if (productType == 1){
             if(budget < 300){
                 count = 1;
@@ -37,12 +39,14 @@ function getBotResponse(input){
                 return "Got it! what size would you prefer (x-small, small, medium, large)?";
             }
         }
+        // budget for laptop **needs work**
         if (productType == 2){
             if(budget < 100){
                 count = 1;
                 return "sorry nothing fits your budget, try again";
             }
         }
+        // Budget for Phone 
         if (productType == 3){
             if(budget < 700){
                 count = 1;
@@ -59,9 +63,13 @@ function getBotResponse(input){
             }else if (budget >= 1300 ){
                 budgetType = 4;
                 return "Got it! what size would you prefer (x-small, small, medium, large)?";
+            }else{
+                count = 1;
+                return "Sorry, didn't seem to understand, please try again :)"
             }
 
         }
+        //Budget for tablet 
         if (productType == 4){
             if(budget < 450){
                 count = 1;
@@ -73,7 +81,11 @@ function getBotResponse(input){
                 budgetType = 2;
                 return "Got it! what size would you prefer (small or large)?";
             }else if (budget >=1100 ){
-
+                budgetType = 3;
+                return "Got it! what size would you prefer (small, medium, large)?";
+            }else{
+                count = 1;
+                return "Sorry, didn't seem to understand, please try again :)"
             }
         }
     }
@@ -88,6 +100,9 @@ function getBotResponse(input){
                 return "The IPhone 13 mini is the device for you! It fits the information you have provided, and more information about this device can be found at the apple website.";
             }else if(input == "large"){
                 return "The IPhone 12 is the device for you! It fits the information you have provided, and more information about this device can be found at the apple website.";
+            }else{
+                count = 2;
+                return "Sorry, didn't seem to understand, please try again :)"
             }
         }else if (productType == 3 && budgetType == 2){
             if(input == "small"){
@@ -96,6 +111,9 @@ function getBotResponse(input){
                 return "The IPhone 13 mini is the device for you! It fits the information you have provided, and more information about this device can be found at the apple website.";
             }else if(input == "large"){
                 return "The IPhone 12 is the device for you! It fits the information you have provided, and more information about this device can be found at the apple website.";
+            }else{
+                count = 2;
+                return "Sorry, didn't seem to understand, please try again :)"
             }
         }else if(productType == 3 && budgetType == 3){
             if (input == "x-small"){
@@ -106,6 +124,9 @@ function getBotResponse(input){
                 return "The IPhone 12, IPhone 13, and IPhone 14 fit this screen size but the Phones cost $850, $1000, $1100 each respectively. A specification comparison can be made on the Apple website to list every one of their differences.";
             }else if(input == "large"){
                 return "The IPhone 14 plus is the device for you! It fits the information you have provided, and more information about this device can be found at the apple website.";
+            }else{
+                count = 2;
+                return "Sorry, didn't seem to understand, please try again :)"
             }
         }else if(productType == 3 && budgetType == 4){
             if (input == "x-small"){
@@ -116,12 +137,35 @@ function getBotResponse(input){
                 return "The IPhone 12, IPhone 13, and IPhone 14 fit this screen size but the Phones cost $850, $1000, $1100 each respectively. A specification comparison can be made on the Apple website to list every one of their differences.";
             }else if(input == "large"){
                 return " Both the IPhone 14 and IPhone 14 Pro Max fit this screen size but the IPhone 14 Pro Max is $300 more than the IPhone 14 Plus. A specification comparison can be made on the Apple website to list every one of their differences.";
+            }else{
+                count = 2;
+                return "Sorry, didn't seem to understand, please try again :)"
             }
         }
 
         // recommendations for tablets
         if(productType == 4 && budgetType == 1){
-            return 
+            return "The IPad Generation 9 and Generation 10 are the devices for you! They fit the information you have provided, and more information about and comparisons between these devices can be found at the apple website.";
+        }else if(productType == 4 && budgetType == 2){
+            if(input == "small"){
+                return "The IPad mini is the device for you! It fits the information you have provided, and more information about this device can be found at the apple website.";
+            }else if (input == "large"){
+                return "The IPad Generation 9 and Generation 10 are the devices for you! They fit the information you have provided, and more information about and comparisons between these devices can be found at the apple website.";
+            }else{
+                count = 2;
+                return "Sorry, didn't seem to understand, please try again :)"
+            }
+        }else if(productType == 4 && budgetType == 3){
+            if(input == "small"){
+                return "The IPad mini is the device for you! It fits the information you have provided, and more information about this device can be found at the apple website."; 
+            }else if (input == "medium"){
+                return "The IPad Generation 9 and Generation 10 are the devices for you! They fit the information you have provided, and more information about and comparisons between these devices can be found at the apple website.";
+            }else if (input == "large"){
+                return "The IPad Pro which comes in the listed sizes is the device for you! It fits the information you have provided, and more information about this device can be found at the apple website.";
+            }else{
+                count = 2;
+                return "Sorry, didn't seem to understand, please try again :)"
+            }
         }
 
     }
